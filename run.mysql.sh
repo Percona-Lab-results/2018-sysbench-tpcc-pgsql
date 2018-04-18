7403f13ee56bdfe0b049931e3751b0354a87dc28
root <root@smblade04.dur.percona.com>
2018-04-18 01:03:22 -0400

    results

---
+++ run.mysql.sh
@@ -0,0 +1,1 @@
+./tpcc.lua  --threads=56 --time=3600 --report-interval=1 --tables=10 --scale=100  --db-driver=mysql --mysql-host=127.0.0.1 run
---
+++ run.pg.sh
@@ -0,0 +1,1 @@
+./tpcc.lua  --threads=56 --time=3600 --report-interval=1 --tables=10 --scale=100  --db-driver=pgsql --pgsql-user=sbtest --pgsql-password=sbtest  --pgsql-db=sbtest run
